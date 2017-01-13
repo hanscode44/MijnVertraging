e158f07d5ecb90add6620687a6e06710e5bcee02
Hans <hans@hotflo.net>
2017-01-13 12:44:12 +0100

    add js to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
 </div> <!--Sluit container-->
+
 <footer>
-<!--    Inhoud komt later-->
+    <!--    Inhoud komt later-->
 </footer>
+
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script src="js/documentready.js"></script>
+
 </body>
 </html>
