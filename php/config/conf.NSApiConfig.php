a948c8023b1cd84e400a1471f6b1567d4abd1988
Hans <hans@hotflo.net>
2017-01-18 15:09:37 +0100

    update

--- php/config/conf.NSApiConfig.php
+++ php/config/conf.NSApiConfig.php
@@ -9,8 +9,8 @@ class NSApiConfig
     public function getConfig()
     {
         return array(
-            'username' => 'hansloos.g1@gmail.com',
-            'password' => '3BcR3nwL2qLk8YF_PHSeNTE0GjZAWLmUhSq7yK-twd-AJQmmOvdxYQ'
+            'username' => '',
+            'password' => ''
         );
     }
 }
